394dc0d44bbafa7b535b3a4693c277e52f9b0584
40Percent <johansonm99@gmail.com>
2023-06-20 11:20:29 -0700

    updated gas test to append all data from gas sensor to csv. proof of concept don't judge me

--- valve_index/gas_uart_test.py
+++ valve_index/gas_uart_test.py
@@ -13,7 +13,7 @@ columns = ['Date', 'Time UTC', 'Latitude+', 'Latitude-', 'Data String Indicator'
            'Pressure (Torr)', 'Temp (K)']
 df = pd.DataFrame(columns=columns)
 
-line = ser.readline().decode('utf-8').strip()  # Read a line from the com port
-
-# Split the line into measurements, assuming they are separated by spaces, removes unneccisary data
-measurements = line.split(' ')[:-55]
+while True:
+    line = ser.readline().decode('utf-8').strip()  # Read a line from the com port
+    measurements = line.split(' ')[:-55] # Split the line into measurements, assuming they are separated by spaces, removes unneccisary data
+    df = df.append(measurements, ignore_index=True)
